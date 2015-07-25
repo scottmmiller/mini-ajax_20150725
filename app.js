3bcdf076520cc80982bfa0d79df119540c09a2fd
Scott Skoot Miller <smmiller.work@gmail.com>
2015-07-25 11:19:55 -0600

    completed step 2: addUser showing to html

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
+   $('#addUser').on('click', function(event) {
+      event.preventDefault();
+      var userName = $('#name').val();
+      var userJob = $('#job').val();
+      return $.ajax({
+         method: "POST",
+         url: 'http://reqr.es/api/users',
+         data: {name: userName, job: userJob},
+         success: function (response) {
+            $('#recentUser').html('<li>' + 'name: ' + response.name + '</li>' +
+               '<li>' + 'job: ' + response.job + '</li>' +
+               '<li>' + 'id: ' + response.id + '</li>' +
+               '<li>' + 'created at: ' + response.createdAt + '</li>')
+         }
+      });
+   });
+
+
+      // ****GET USERS****
    var insertData = function(array) {
       for (var i = 0; i < array.length; i++) {
          $("#userInfo" + (i + 1)).html('<div>' + 'User Info: ' +
@@ -20,5 +39,5 @@ $(document).ready(function() {
          }
       });
    });
-
+      // ************************
 });
